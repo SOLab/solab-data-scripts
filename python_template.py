8b2cbb606b6952e628892bf1103b9c62ab17420c
Kate <blood_garpia@mail.ru>
2014-02-28 16:21:52 +0400

    add en-comments in python_template.py

--- python_template.py
+++ python_template.py
@@ -2,28 +2,34 @@
 from pydap.client import open_url
 
 # исследуемый геофизический параметр
+# in question geophysical parameter
 variable = 'atmosphere_water_vapor_content'
 
 # массив ссылок на выбранные гранулы
+# an array of references to the selected granules
 granules = [
     'http://opendap.solab.rshu.ru:8080/opendap/allData/SOLAB_AMSRE_L2_NN/2011/10/SOLAB_AMSRE_L2_NN_20111004_060306_20111004_065147_232_A_v1.nc',
     'http://opendap.solab.rshu.ru:8080/opendap/allData/SOLAB_AMSRE_L2_NN/2011/10/SOLAB_AMSRE_L2_NN_20111001_021430_20111001_030424_195_A_v1.nc'
 ]
 
 # подстановка широты и долготы выбранного региона в индексах массива данных
+# substitution of latitude and longitude of the selected region in indices of the data array
 min_lat = 0
 max_lat = 1948
 min_lon = 0
 max_lon = 195
 
 # шаг сетки
+# grid step
 step = 1
 
 # цикл по выбранным гранулам
+# iteration in selected granules
 for granule in granules:
     dataset = open_url(granule)
     selection = dataset[variable][min_lat:step:max_lat][min_lon:step:max_lon]
     # добавьте процедуры обработки данных ниже
+    # add data processing procedures below
     print selection
 
        
