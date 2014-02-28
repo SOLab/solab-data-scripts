c977186952ab74270f986ead438ccbea2140aa30
Kate <blood_garpia@mail.ru>
2014-02-28 17:13:58 +0400

    save matlab_template.m in utf-8

--- matlab_template.m
+++ matlab_template.m
@@ -1,36 +1,36 @@
-%исследуемый геофизический параметр
+%РёСЃСЃР»РµРґСѓРµРјС‹Р№ РіРµРѕС„РёР·РёС‡РµСЃРєРёР№ РїР°СЂР°РјРµС‚СЂ
 %in question geophysical parameter
 variable = 'atmosphere_water_vapor_content';
 
-%массив ссылок на выбранные гранулы
+%РјР°СЃСЃРёРІ СЃСЃС‹Р»РѕРє РЅР° РІС‹Р±СЂР°РЅРЅС‹Рµ РіСЂР°РЅСѓР»С‹
 %an array of references to the selected granules
 granules = {
     'http://opendap.solab.rshu.ru:8080/opendap/allData/SOLAB_AMSRE_L2_NN/2011/10/SOLAB_AMSRE_L2_NN_20111004_060306_20111004_065147_232_A_v1.nc',
     'http://opendap.solab.rshu.ru:8080/opendap/allData/SOLAB_AMSRE_L2_NN/2011/10/SOLAB_AMSRE_L2_NN_20111001_021430_20111001_030424_195_A_v1.nc'
 };
 
-%подстановка широты и долготы выбранного региона в индексах массива данных
+%РїРѕРґСЃС‚Р°РЅРѕРІРєР° С€РёСЂРѕС‚С‹ Рё РґРѕР»РіРѕС‚С‹ РІС‹Р±СЂР°РЅРЅРѕРіРѕ СЂРµРіРёРѕРЅР° РІ РёРЅРґРµРєСЃР°С… РјР°СЃСЃРёРІР° РґР°РЅРЅС‹С…
 %substitution of latitude and longitude of the selected region in indices of the data array
 min_lat = 1;
 max_lat = 1948;
 min_lon = 1;
 max_lon = 196;
 
-%шаг сетки
+%С€Р°Рі СЃРµС‚РєРё
 %grid step
 step = 1;
 
-% создание структуры для хранения извлекаемых данных
+% СЃРѕР·РґР°РЅРёРµ СЃС‚СЂСѓРєС‚СѓСЂС‹ РґР»СЏ С…СЂР°РЅРµРЅРёСЏ РёР·РІР»РµРєР°РµРјС‹С… РґР°РЅРЅС‹С…
 % creating a structure for storing data that is retrieved
 selection = struct([]);
 
-%цикл по выбранным гранулам
+%С†РёРєР» РїРѕ РІС‹Р±СЂР°РЅРЅС‹Рј РіСЂР°РЅСѓР»Р°Рј
 % iteration in selected granules
 for i = 1:length(granules)
     dataset = readNetCDF(char(granules{i}));
     selection{i} = dataset.(variable).data(min_lat:step:max_lat, min_lon:step:max_lon);
     
-    % вывод данных
+    % РІС‹РІРѕРґ РґР°РЅРЅС‹С…
     % data output
     disp(selection{i}) 
 end
