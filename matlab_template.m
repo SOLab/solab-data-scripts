8f236ce689b356d3443eb46125337d87cae67462
Kate <blood_garpia@mail.ru>
2014-03-06 13:05:38 +0400

    fix matlab_template.m and delete matlab_template2.m

--- matlab_template.m
+++ matlab_template.m
@@ -1,20 +1,22 @@
 %исследуемый геофизический параметр
 %in question geophysical parameter
-variable = 'atmosphere_water_vapor_content';
+parameters = {
+	'atmosphere_water_vapor_content',
+};
 
 %массив ссылок на выбранные гранулы
 %an array of references to the selected granules
 granules = {
     'http://opendap.solab.rshu.ru:8080/opendap/allData/SOLAB_AMSRE_L2_NN/2011/10/SOLAB_AMSRE_L2_NN_20111004_060306_20111004_065147_232_A_v1.nc',
-    'http://opendap.solab.rshu.ru:8080/opendap/allData/SOLAB_AMSRE_L2_NN/2011/10/SOLAB_AMSRE_L2_NN_20111001_021430_20111001_030424_195_A_v1.nc'
+    'http://opendap.solab.rshu.ru:8080/opendap/allData/SOLAB_AMSRE_L2_NN/2011/10/SOLAB_AMSRE_L2_NN_20111001_021430_20111001_030424_195_A_v1.nc',
 };
 
 %подстановка широты и долготы выбранного региона в индексах массива данных
 %substitution of latitude and longitude of the selected region in indices of the data array
 min_lat = 1;
-max_lat = 1948;
+max_lat = 195;
 min_lon = 1;
-max_lon = 196;
+max_lon = 1947;
 
 %шаг сетки
 %grid step
@@ -28,9 +30,11 @@ selection = struct([]);
 % iteration in selected granules
 for i = 1:length(granules)
     dataset = readNetCDF(char(granules{i}));
-    selection{i} = dataset.(variable).data(min_lat:step:max_lat, min_lon:step:max_lon);
-    
-    % вывод данных
-    % data output
-    disp(selection{i})  
+
+    for j = 1:length(parameters)
+	    selection{j, i} = dataset.(parameters{j}).data(min_lon:step:max_lon, min_lat:step:max_lat);
+	    % вывод данных
+	    % data output
+	    disp(selection{j, i})
+	end  
 end
