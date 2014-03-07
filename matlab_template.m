0f184c120495af61a169f4d3fc97e9a88e6649af
Kate <blood_garpia@mail.ru>
2014-03-07 14:05:22 +0400

    fix matlab_template.m

--- matlab_template.m
+++ matlab_template.m
@@ -6,17 +6,14 @@ parameters = {
 
 %массив ссылок на выбранные гранулы
 %an array of references to the selected granules
-granules = {
-    'http://opendap.solab.rshu.ru:8080/opendap/allData/SOLAB_AMSRE_L2_NN/2011/10/SOLAB_AMSRE_L2_NN_20111004_060306_20111004_065147_232_A_v1.nc',
-    'http://opendap.solab.rshu.ru:8080/opendap/allData/SOLAB_AMSRE_L2_NN/2011/10/SOLAB_AMSRE_L2_NN_20111001_021430_20111001_030424_195_A_v1.nc',
-};
 
-%подстановка широты и долготы выбранного региона в индексах массива данных
-%substitution of latitude and longitude of the selected region in indices of the data array
-min_lat = 1;
-max_lat = 195;
-min_lon = 1;
-max_lon = 1947;
+granules = [struct('url', 'http://opendap.solab.rshu.ru:8080/opendap/allData/SOLAB_AMSRE_L2_NN/2011/10/SOLAB_AMSRE_L2_NN_20111004_060306_20111004_065147_232_A_v1.nc',...
+    'min_lat', 1, 'max_lat', 196, 'min_lon', 1, 'max_lon', 1948), ...
+    struct('url', 'http://opendap.solab.rshu.ru:8080/opendap/allData/SOLAB_AMSRE_L2_NN/2011/10/SOLAB_AMSRE_L2_NN_20111001_021430_20111001_030424_195_A_v1.nc', ...
+    'min_lat', 1, 'max_lat', 196, ...
+    'min_lon', 1, ... 
+    'max_lon', 1948)...
+]
 
 %шаг сетки
 %grid step
@@ -29,10 +26,11 @@ selection = struct([]);
 %цикл по выбранным гранулам
 % iteration in selected granules
 for i = 1:length(granules)
-    dataset = readNetCDF(char(granules{i}));
+    granules(i).url
+    dataset = readNetCDF(char(granules(i).url));
 
     for j = 1:length(parameters)
-	    selection{j, i} = dataset.(parameters{j}).data(min_lon:step:max_lon, min_lat:step:max_lat);
+	    selection{j, i} = dataset.(parameters{j}).data(granules(i).min_lon:step:granules(i).max_lon, granules(i).min_lat:step:granules(i).max_lat);
 	    % вывод данных
 	    % data output
 	    disp(selection{j, i})
