80307809002886adfc8efd00efa752953083d9a0
Kate <blood_garpia@mail.ru>
2014-03-07 13:16:07 +0400

    fix python_template.py

--- python_template.py
+++ python_template.py
@@ -1,36 +1,29 @@
+# v0.3, 05.03.2014, SOLab
 # OPeNDAP client - http://pydap.org
 from pydap.client import open_url
 
 # исследуемый геофизический параметр
-# in question geophysical parameter
-variable = 'atmosphere_water_vapor_content'
-
-# массив ссылок на выбранные гранулы
-# an array of references to the selected granules
-granules = [
-    'http://opendap.solab.rshu.ru:8080/opendap/allData/SOLAB_AMSRE_L2_NN/2011/10/SOLAB_AMSRE_L2_NN_20111004_060306_20111004_065147_232_A_v1.nc',
-    'http://opendap.solab.rshu.ru:8080/opendap/allData/SOLAB_AMSRE_L2_NN/2011/10/SOLAB_AMSRE_L2_NN_20111001_021430_20111001_030424_195_A_v1.nc'
+parameters = [
+    'atmosphere_water_vapor_content', 'wind_speed_lf', 'sea_surface_temperature', 'atmosphere_cloud_liquid_water_content', 'atmospheric_absorption_at_11GHz'
 ]
 
-# подстановка широты и долготы выбранного региона в индексах массива данных
-# substitution of latitude and longitude of the selected region in indices of the data array
-min_lat = 0
-max_lat = 1948
-min_lon = 0
-max_lon = 195
+# массив ссылок на выбранные гранулы, а также широта и долгота выбранного региона в индексах массива данных
+granules = [
+    {'url': 'http://opendap.solab.rshu.ru:8080/opendap/allData/SOLAB_AMSRE_L2_NN/2011/10/SOLAB_AMSRE_L2_NN_20111004_051344_20111004_060336_232_D_v1.nc', 'min_lat': 0, 'max_lat': 195, 'min_lon': 0, 'max_lon': 1995}, 
+    {'url': 'http://opendap.solab.rshu.ru:8080/opendap/allData/SOLAB_AMSRE_L2_NN/2011/10/SOLAB_AMSRE_L2_NN_20111004_042418_20111004_051414_216_A_v1.nc', 'min_lat': 0, 'max_lat': 195, 'min_lon': 0, 'max_lon': 1997}
+]
 
 # шаг сетки
-# grid step
 step = 1
 
 # цикл по выбранным гранулам
-# iteration in selected granules
 for granule in granules:
-    dataset = open_url(granule)
-    selection = dataset[variable][min_lat:step:max_lat][min_lon:step:max_lon]
-    # добавьте процедуры обработки данных ниже
-    # add data processing procedures below
-    print selection
+    dataset = open_url(granule['url'])
+    # цикл по геофизическим параметрам
+    for parameter in parameters:
+        selection = dataset[parameter][granule['min_lat']:step:granule['max_lat']][granule['min_lon']:step:granule['max_lon']]
+        # добавьте процедуры обработки данных ниже
+        print selection
 
        
     
